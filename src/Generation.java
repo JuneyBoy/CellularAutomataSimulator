436519618a26c7aa28e8b4888c37a9b33c8c0394
JuneyBoy <arjun.ganesan@ou.edu>
2020-04-01 11:07:51 -0500

    Wrote Generation.java Class
    Wrote all the methods of the Generation.java class from the UML diagram, but I'm pretty sure I messed up the IllegalArgumentException

--- src/Generation.java
+++ src/Generation.java
@@ -1,4 +1,48 @@
 
 public class Generation {
+	
+	private Cell[] cells;
+	
+	public Generation(CellState[] states) {
+		cells = new Cell[states.length];
+		int i = 0;
+		for(CellState state : states) {
+			cells[i] = new Cell(state);
+			++i;
+		}
+	}
+	
+	public Generation(String string) {
+		cells = new Cell[string.length()];
+		try {
+			for(int i = 0; i < string.length(); ++i) {
+				cells[i] = new Cell(CellState.getState(string.charAt(i)));
+			}
+		}
+		catch(IllegalArgumentException e) {
+			System.out.println("Character in String not a valid symbol for CellState");
+		}
+	}
+	
+	public Generation(Cell[] cells) {
+		this.cells = cells;
+	}
+	
+	public int size() {
+		return cells.length;
+	}
+	
+	public Cell getCell(int idx) {
+		return cells[idx];
+	}
+	
+	public String toString() {
+		String returnString = "";
+		
+		for(Cell cell : cells) {
+			returnString.concat(cell.toString());
+		}
+		return returnString;
+	}
 
 }
