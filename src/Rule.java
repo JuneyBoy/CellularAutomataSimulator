c7677afa4a12891ccb449fec2ddfc12fccf0b395
JuneyBoy <arjun.ganesan@ou.edu>
2020-04-01 11:24:04 -0500

    Wrote Rule.java class
    Wrote all the methods of the Rule.java class from the UML diagram. I might have written the Generation evolve method incorrectly

--- src/Rule.java
+++ src/Rule.java
@@ -1,4 +1,32 @@
 
 public abstract class Rule {
+	
+	private int ruleNum;
+	
+	protected Rule(int ruleNum) {
+		this.ruleNum = ruleNum;
+	}
+	
+	public int getRuleNum() {
+		return ruleNum;
+	}
+	
+	public Generation evolve (Generation gen, BoundaryConditions bc) {
+		Cell[] newGen = new Cell[gen.size()];
+		
+		for(int i = 0; i < newGen.length; ++i) {
+			Cell[] neighborhood = this.getNeighborhood(i, gen, bc);
+			newGen[i] = this.evolve(neighborhood);
+		}
+		return new Generation(newGen);
+	}
+	
+	public abstract int getNumSubrules();
+	
+	public abstract Cell[] getNeighborhood(int cellIdx, Generation gen, BoundaryConditions bc);
+	
+	public abstract EvolvedCell evolve(Cell[] neighborhood);
+	
+	public abstract String toString();
 
 }
