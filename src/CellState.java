3da11f8c95d4205108d1f23f67d1d9eb4a0dccea
JuneyBoy <arjun.ganesan@ou.edu>
2020-04-01 10:31:07 -0500

    Almost Finished CellState
    Wrote most of the CellState.java file, but  I still need to finish the constructor and I am a little confused on how to use the "symbol" field

--- src/CellState.java
+++ src/CellState.java
@@ -1,4 +1,27 @@
-
+import java.util.HashMap;
 public enum CellState {
+	
+	ON('.'), 
+	OFF('O');
+	
+	private char symbol;
+	private static final HashMap<Character, CellState> SYMBOL_TO_MAP = new HashMap<>();
+	
+	static {
+		SYMBOL_TO_MAP.put('.', ON);
+		SYMBOL_TO_MAP.put('O', OFF);
+	}
+	
+	private CellState(char symbol) {
+		this.symbol = symbol;
+	}
+	
+	public static CellState getState(char symbol) {
+		return SYMBOL_TO_MAP.get(symbol);
+	}
+	
+	public String toString() {
+		return this.name();
+	}
 
 }
