9218731e75a01e4b0567938844c400ca03b3b5cb
JuneyBoy <arjun.ganesan@ou.edu>
2020-04-02 09:34:16 -0500

    Tested TotalisticRule class
    Looks like easy example of the Elementary and Totalistic Rule classes work

--- src/Driver.java
+++ src/Driver.java
@@ -2,16 +2,15 @@
 public class Driver {
 
 	public static void main(String[] args)throws InvalidRuleNumException {
-		ElementaryRule rule = new ElementaryRule(1);
+		TotalisticRule rule = new TotalisticRule(22);
 		BoundaryConditions bc = new CircularBoundaryConditions();
 		Cell offCell = new Cell(CellState.OFF);
 		Cell onCell = new Cell(CellState.ON);
-		Cell[] init = {offCell, offCell, onCell, offCell, offCell};
+		Cell[] init = {offCell, offCell, offCell, onCell, onCell, onCell, onCell, onCell, offCell, offCell, offCell};
 		Generation initGen = new Generation(init);
 		
 		Automaton a = new Automaton(rule, initGen, bc);
 		a.evolve(1);
 		System.out.println(a.getHistory());
-		
 	}
 }
