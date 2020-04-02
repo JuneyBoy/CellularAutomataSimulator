addf5ab15b2441b38232f63f39b5708b2fe7b977
JuneyBoy <arjun.ganesan@ou.edu>
2020-04-01 19:08:13 -0500

    Wrote Both Voundary Condition Classes
    Wrote the methods for both classes from the UML diagram

--- src/CircularBoundaryConditions.java
+++ src/CircularBoundaryConditions.java
@@ -1,4 +1,14 @@
 
 public class CircularBoundaryConditions {
+	
+	public CircularBoundaryConditions() {
+		
+	}
+	
+	public Cell getNeighbor(int cellIdx, int offset, Generation gen) {
+		int cellsInGen = gen.size();
+		Cell returnCell = gen.getCell((cellIdx + offset) % cellsInGen);
+		return returnCell;
+	}
 
 }
--- src/FixedBoundaryConditions.java
+++ src/FixedBoundaryConditions.java
@@ -1,4 +1,36 @@
 
 public class FixedBoundaryConditions {
+	
+	private CellState leftBoundaryCondition;
+	private CellState rightBoundaryCondition;
+	
+	public FixedBoundaryConditions(CellState left, CellState right) {
+		leftBoundaryCondition = left;
+		rightBoundaryCondition = right;
+	}
+	
+	public CellState getLeftState() {
+		return leftBoundaryCondition;
+	}
+	
+	public CellState getRightState() {
+		return rightBoundaryCondition;
+	}
+	
+	public Cell getNeighbor(int cellIdx, int offset, Generation gen) {
+		Cell returnCell;
+		int cellsInGen = gen.size();
+		int neighborNum = cellIdx + offset;
+		if(neighborNum < 0) {
+			returnCell = new Cell(this.getLeftState());
+		}
+		else if(neighborNum >= cellsInGen) {
+			returnCell = new Cell(this.getRightState());
+		}
+		else {
+			returnCell = gen.getCell(neighborNum);
+		}
+		return returnCell;
+	}
 
 }
