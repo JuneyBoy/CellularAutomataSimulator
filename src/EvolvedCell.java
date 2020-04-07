8de6ec88936560f10fd1e7314a9cfcf3c4f0dd76
JuneyBoy <arjun.ganesan@ou.edu>
2020-04-06 23:04:42 -0500

    Commented EvolvedCell
    Wrote javadoc comments

--- src/EvolvedCell.java
+++ src/EvolvedCell.java
@@ -1,13 +1,29 @@
-
+/**
+ * This class extends the Cell class and will make up any Generation of the Automaton that has been evolved.
+ * The extra data member holds which subrule it was evolved by.
+ * 
+ * @author Arjun Ganesan
+ * @version 1.0
+ *
+ */
 public class EvolvedCell extends Cell{
 	
 	private int subruleNum;
 	
+	/**
+	 * 
+	 * @param state sets the state of the EvolvedCell object
+	 * @param subruleNum stores the subrule of the Rule that applied to this EvolvedCell
+	 */
 	public EvolvedCell(CellState state, int subruleNum) {
 		super(state);
 		this.subruleNum = subruleNum; 
 	}
 	
+	/**
+	 * 
+	 * @return the subrule that the EvolvedCell evolved by
+	 */
 	public int getSubruleNum() {
 		return subruleNum;
 	}
