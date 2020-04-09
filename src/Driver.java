8e1dbd2ec7cd548884d0fb199d84eca8c9666cbf
JuneyBoy <arjun.ganesan@ou.edu>
2020-04-08 23:43:08 -0500

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 public class Driver {
 
 	public static void main(String[] args)throws InvalidRuleNumException, InvalidStepNumException {
-		Rule rule = new ElementaryRule(15);
+		Rule rule = new ElementaryRule(3);
 		BoundaryConditions bc = new CircularBoundaryConditions();
-		Generation initGen = new Generation("..O..");
+		Generation initGen = new Generation("......O......");
 		
 		System.out.println(rule.toString());
 		System.out.println();
 		Automaton a = new Automaton(rule, initGen, bc);
-		a.evolve(1);
+		a.evolve(10);
 		
 		System.out.println(a.getHistory());
 		System.out.println();
