f8bbf01fc2cfb0f8eaefc51fdc8aba61acbdda2c
JuneyBoy <arjun.ganesan@ou.edu>
2020-07-28 21:40:45 -0500

    Updated FBC and added comments to CBC and FBC

--- src/CircularBoundaryConditions.java
+++ src/CircularBoundaryConditions.java
@@ -42,40 +42,37 @@ public class CircularBoundaryConditions implements BoundaryConditions{
 	}
 	
 	/**
+	 * 
 	 * @param cellIdx the index of the Cell whose neighbor we are trying to access
-	 * @param offset the index of the neighbor relative to the Cell
-	 * @param gen the Generation
-	 * @return the Cell with index cellIdx + offset
+	 * @param rowOffset the row of the neighbor relative to the Cell
+	 * @param colOffset the col of the neighbor relative to the Cell
+	 * @param gen the 2D Generation
+	 * @return the Cell with indices (cellIdx + rowOffset, cellIdx + colOffset)
 	 */
 	public Cell getNeighbor(int cellIdx, int rowOffset, int colOffset, TwoDGeneration gen) {
+		
+		Cell returnCell;
 		
 		int rowsInGen = gen.numOfRows();
 		int colsInGen = gen.numOfCols();
 		
 		int rowOfNeighbor = (cellIdx + rowOffset) % rowsInGen;
 		int colOfNeighbor = (cellIdx + colOffset) % colsInGen;
+	
 		
-		Cell returnCell;
-		
-		//if the sum of the cellIdx and offset is negative, that means the Cell is "too left",
+		//if the sum of the cellIdx and row offset is negative, that means the Cell is "too high",
+		//so we begin accessing the Cells starting from the bottom
+		if(rowOfNeighbor < 0) {
+			rowOfNeighbor += rowsInGen;
+		}
+		//if the sum of the cellIdx and col offset is negative, that means the Cell is "too left",
 		//so we begin accessing the Cells starting from the right
-		if(rowOfNeighbor < 0 && colOfNeighbor < 0) {
-			returnCell = gen.getCell((rowsInGen + rowOfNeighbor), (colsInGen + colOfNeighbor));
-			return returnCell;
-		}
-		
-		else if(rowOfNeighbor < 0 && colOfNeighbor > 0) {
-			returnCell = gen.getCell((rowsInGen + rowOfNeighbor), (colOfNeighbor));
-			return returnCell;
-		}
-		
-		else if(rowOfNeighbor > 0 && colOfNeighbor < 0) {
-			returnCell = gen.getCell((rowOfNeighbor), (colsInGen + colOfNeighbor));
-			return returnCell;
+		else if(colOfNeighbor < 0) {
+			colOfNeighbor += colsInGen;
 		}
 		
 		//we use the modulo operator to take care of the case where the sum of cellIdx and offset is greater
-		//than than the size of the Generation and begin wrapping around the left of the Generation
+		//than than the size of the Generation and begin wrapping around the left or top of the Generation
 		returnCell = gen.getCell(rowOfNeighbor, colOfNeighbor);
 		return returnCell;
 	}
--- src/FixedBoundaryConditions.java
+++ src/FixedBoundaryConditions.java
@@ -10,15 +10,31 @@ public class FixedBoundaryConditions implements BoundaryConditions{
 	
 	private CellState leftBoundaryCondition;
 	private CellState rightBoundaryCondition;
+	private CellState aboveBoundaryCondition;
+	private CellState belowBoundaryCondition;
 	
 	/**
-	 * 
+	 * Constructor is for 1D Automata
 	 * @param left is the CellState for all Cells that are considered left of the left most Cell
 	 * @param right is the CellState for all Cells that are considered right of the right most Cell
 	 */
 	public FixedBoundaryConditions(CellState left, CellState right) {
 		leftBoundaryCondition = left;
 		rightBoundaryCondition = right;
+	}
+	
+	/**
+	 * Constructor is for 2D Automata
+	 * @param left is the CellState for all Cells that are considered left of the left most column
+	 * @param right is the CellState for all Cells that are considered right of the right most column
+	 * @param above is the CellState for all Cells that are considered above the top row
+	 * @param right is the CellState for all Cells that are considered below the bottow row
+	 */
+	public FixedBoundaryConditions(CellState left, CellState right, CellState above, CellState below) {
+		leftBoundaryCondition = left;
+		rightBoundaryCondition = right;
+		aboveBoundaryCondition = above;
+		belowBoundaryCondition = below;
 	}
 	
 	/**
@@ -35,6 +51,22 @@ public class FixedBoundaryConditions implements BoundaryConditions{
 	 */
 	public CellState getRightState() {
 		return rightBoundaryCondition;
+	}
+	
+	/**
+	 * 
+	 * @return the aboveBoundaryCondition that was set in the constructor
+	 */
+	public CellState getAboveState() {
+		return aboveBoundaryCondition;
+	}
+	
+	/**
+	 * 
+	 * @return the belowBoundaryCondition that was set in the constructor
+	 */
+	public CellState getBelowState() {
+		return belowBoundaryCondition;
 	}
 	
 	/**
@@ -64,6 +96,54 @@ public class FixedBoundaryConditions implements BoundaryConditions{
 		else {
 			returnCell = gen.getCell(neighborNum);
 		}
+		return returnCell;
+	}
+	
+	/**
+	 * 
+	 * @param cellIdx the index of the Cell whose neighbor we are trying to access
+	 * @param rowOffset the row of the neighbor relative to the Cell
+	 * @param colOffset the col of the neighbor relative to the Cell
+	 * @param gen the 2D Generation
+	 * @return the Cell with indices (cellIdx + rowOffset, cellIdx + colOffset)
+	 */
+	public Cell getNeighbor(int cellIdx, int rowOffset, int colOffset, TwoDGeneration gen) {
+		
+		Cell returnCell;
+		int rowsInGen = gen.numOfRows();
+		int colsInGen = gen.numOfCols();
+		
+		int rowOfNeighbor = cellIdx + rowOffset;
+		int colOfNeighbor = cellIdx + colOffset;
+		
+		//if the sum of the cellIdx and row offset is negative, that means the Cell is "too left",
+		//so the state of the neighbor is set to the leftBoundaryCondition
+		if(rowOfNeighbor < 0) {
+			returnCell = new Cell(this.getLeftState());
+		}
+		//if the sum of the cellIdx and row offset is greater than the # of Cells in the generation
+		//that means the Cell is "too right", so the state of the neighbor is set to 
+		//the rightBoundaryCondition
+		else if(rowOfNeighbor >= rowsInGen) {
+			returnCell = new Cell(this.getRightState());
+		}
+		//if the sum of the cellIdx and col offset is negative, that means the Cell is "too left",
+		//so the state of the neighbor is set to the leftBoundaryCondition
+		else if(colOfNeighbor < 0) {
+			returnCell = new Cell(this.belowBoundaryCondition);
+		}
+		//if the sum of the cellIdx and col offset is greater than the # of Cells in the generation
+		//that means the Cell is "too right", so the state of the neighbor is set to 
+		//the rightBoundaryCondition
+		else if(colOfNeighbor >= colsInGen) {
+			returnCell = new Cell(this.aboveBoundaryCondition);
+		}
+		
+		//otherwise, the neighbor is simply the Cell at the specified indices
+		else {
+			returnCell = gen.getCell(rowOfNeighbor, colOfNeighbor);
+		}
+		
 		return returnCell;
 	}
 
